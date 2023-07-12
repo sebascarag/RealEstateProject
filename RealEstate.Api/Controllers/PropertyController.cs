b6bf3a193c1153ae7fb00613495855fb2840b348
sebascarag <sebastiansca10@gmail.com>
2023-07-12 12:58:14 -0500

    PropertyController added

---
+++ RealEstate.Api/Controllers/PropertyController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Application.Properties.Command;
+
+namespace RealEstate.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PropertyController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Post(CreatePropertyCommand.Request request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
