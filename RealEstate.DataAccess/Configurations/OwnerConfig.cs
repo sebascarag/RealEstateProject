56acf771365e034d23b6e34a6d854e952b9ef0dc
sebascarag <sebastiansca10@gmail.com>
2023-07-13 12:05:52 -0500

    Owner seed data added

--- RealEstate.DataAccess/Configurations/OwnerConfig.cs
+++ RealEstate.DataAccess/Configurations/OwnerConfig.cs
@@ -8,7 +8,58 @@ namespace RealEstate.DataAccess.Configurations
     {
         public void Configure(EntityTypeBuilder<Owner> builder)
         {
+            // relations
             builder.HasMany(b => b.Properties);
+
+            // seeds
+            var now = DateTime.UtcNow;
+            builder.HasData(
+                new Owner
+                {
+                    Id = 1,
+                    Name = "Sheri D. Coffelt",
+                    Address = "3071 Todds Lane San Antonio, TX 78205",
+                    Photo = "984bb1a4-46d8-4fbd-b9b1-fa78d395be7b.png",
+                    Birthday = new DateTime(1982, 1, 27),
+                    Active = true,
+                    CreatedBy = "System",
+                    CreatedOn = now,
+                    ModifiedBy = "System",
+                    ModifiedOn = now,
+                }
+            );
+
+            builder.HasData(
+                new Owner
+                {
+                    Id = 2,
+                    Name = "Donnie K. Russell",
+                    Address = "1641 Despard Street Atlanta, GA 30309",
+                    Photo = "ffc8ea6a-10c6-4997-916c-55b20910158c.png",
+                    Birthday = new DateTime(1994, 5, 29),
+                    Active = true,
+                    CreatedBy = "System",
+                    CreatedOn = now,
+                    ModifiedBy = "System",
+                    ModifiedOn = now,
+                }
+            );
+
+            builder.HasData(
+                new Owner
+                {
+                    Id = 3,
+                    Name = "Jeff S. Nelson",
+                    Address = "1271 Hillcrest Circle Golden Valley, MN 55427",
+                    Photo = "d73beddb-805f-47ca-baac-d9a2dbfb92ec.png",
+                    Birthday = new DateTime(1986, 12, 05),
+                    Active = true,
+                    CreatedBy = "System",
+                    CreatedOn = now,
+                    ModifiedBy = "System",
+                    ModifiedOn = now,
+                }
+            );
         }
     }
 }
---
+++ RealEstate.DataAccess/Migrations/20230713155415_RealEstateSeedInitial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RealEstate.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RealEstateSeedInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Owners",
+                columns: new[] { "Id", "Active", "Address", "Birthday", "CreatedBy", "CreatedOn", "ModifiedBy", "ModifiedOn", "Name", "Photo" },
+                values: new object[,]
+                {
+                    { 1, true, "3071 Todds Lane San Antonio, TX 78205", new DateTime(1982, 1, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "Sheri D. Coffelt", "984bb1a4-46d8-4fbd-b9b1-fa78d395be7b.png" },
+                    { 2, true, "1641 Despard Street Atlanta, GA 30309", new DateTime(1994, 5, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "Donnie K. Russell", "ffc8ea6a-10c6-4997-916c-55b20910158c.png" },
+                    { 3, true, "1271 Hillcrest Circle Golden Valley, MN 55427", new DateTime(1986, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "System", new DateTime(2023, 7, 13, 15, 54, 15, 119, DateTimeKind.Utc).AddTicks(6109), "Jeff S. Nelson", "d73beddb-805f-47ca-baac-d9a2dbfb92ec.png" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
