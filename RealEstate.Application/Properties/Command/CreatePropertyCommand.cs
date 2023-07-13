f7fb27a9941d8bb841b523f65270f9799cd8a7de
sebascarag <sebastiansca10@gmail.com>
2023-07-13 12:13:13 -0500

    Command class names adjusted

--- RealEstate.Application/Properties/Command/CreatePropertyCommand.cs
+++ RealEstate.Application/Properties/Command/CreatePropertyCommand.cs
@@ -1,76 +1,73 @@
 ﻿using FluentValidation;
 using MediatR;
+using RealEstate.Application.Contracts;
 using RealEstate.Application.Wrappers;
-using RealEstate.DataAccess.Repository;
 using RealEstate.Domain.Entities;
 
 namespace RealEstate.Application.Properties.Command
 {
-    public class CreatePropertyCommand
+    public record CreatePropertyCommandRequest : IRequest<bool>
     {
-        public class Request : IRequest<bool>
+        public string Name { get; init; }
+        public string Address { get; init; }
+        public decimal Price { get; init; }
+        public int Year { get; init; }
+        public int OwnerId { get; init; }
+    }
+
+    public class CreatePropertyCommandValidation : AbstractValidator<CreatePropertyCommandRequest>
+    {
+        private readonly IRepository<Owner> _ownerRepo;
+
+        public CreatePropertyCommandValidation(IRepository<Owner> ownerRepo)
         {
-            public string Name { get; set; }
-            public string Address { get; set; }
-            public decimal Price { get; set; }
-            public int Year { get; set; }
-            public int OwnerId { get; set; }
+            _ownerRepo = ownerRepo;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .MaximumLength(100).WithMessage("{PropertyName} too large, must not exceed 100 characters");
+            RuleFor(x => x.Address)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .MaximumLength(255).WithMessage("{PropertyName} too large, must not exceed 255 characters");
+            RuleFor(x => x.Price)
+                .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} must be positive value");
+            RuleFor(x => x.OwnerId)
+                .MustAsync(OwnerExist).WithMessage("{PropertyName} doesn't exist");
         }
 
-        public class Validation : AbstractValidator<Request>
+        public async Task<bool> OwnerExist(int ownerId, CancellationToken cancellationToken)
         {
-            private readonly IRepository<Owner> _ownerRepo;
+            return await _ownerRepo.GetByIdAsync(ownerId, cancellationToken) != null;
+        }
+    }
 
-            public Validation(IRepository<Owner> ownerRepo)
-            {
-                _ownerRepo = ownerRepo;
+    public class CreatePropertyCommandHandler : IRequestHandler<CreatePropertyCommandRequest, bool>
+    {
+        private readonly IRepository<Property> _propertyRepo;
 
-                RuleFor(x => x.Name)
-                    .NotEmpty().WithMessage("{PropertyName} is required")
-                    .MaximumLength(100).WithMessage("{PropertyName} too large, must not exceed 100 characters");
-                RuleFor(x => x.Address)
-                    .NotEmpty().WithMessage("{PropertyName} is required")
-                    .MaximumLength(255).WithMessage("{PropertyName} too large, must not exceed 255 characters");
-                RuleFor(x => x.Price)
-                    .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} must be positive value");
-                RuleFor(x => x.OwnerId)
-                    .MustAsync(OwnerExist).WithMessage("{PropertyName} doesn't exist");
-            }
-
-            public async Task<bool> OwnerExist(int ownerId, CancellationToken cancellationToken)
-            {
-                return await _ownerRepo.GetByIdAsync(ownerId, cancellationToken) != null;
-            }
+        public CreatePropertyCommandHandler(IRepository<Property> propertyRepo)
+        {
+            _propertyRepo = propertyRepo;
         }
 
-        public class Handler : IRequestHandler<Request, bool>
+        public async Task<bool> Handle(CreatePropertyCommandRequest request, CancellationToken cancellationToken)
         {
-            private readonly IRepository<Property> _propertyRepo;
-
-            public Handler(IRepository<Property> propertyRepo)
+            var property = new Property
             {
-                _propertyRepo = propertyRepo;
-            }
-
-            public Task<bool> Handle(Request request, CancellationToken cancellationToken)
-            {
-                var property = new Property
-                {
-                    Name = request.Name,
-                    Address = request.Address,
-                    Price = request.Price,
-                    Year = request.Year,
-                    OwnerId = request.OwnerId,
-                    CodeInternal = Guid.NewGuid().ToString(),
-                    Active = true
-                };
-
-                _propertyRepo.Add(property);
-                if (_propertyRepo.Save())
-                    return Task.FromResult(true);
-                else
-                    throw new ApiException("Property could not be save");
-            }
+                Name = request.Name,
+                Address = request.Address,
+                Price = request.Price,
+                Year = request.Year,
+                OwnerId = request.OwnerId,
+                CodeInternal = Guid.NewGuid().ToString(),
+                Active = true
+            };
+            _propertyRepo.Add(property);
+            var result = await _propertyRepo.SaveAsync();
+            if (result)
+                return result;
+            else
+                throw new ApiException("Property could not be save");
         }
     }
 }
