2463a6a14815dfd6a9784685585dccbdc59f328a
sebascarag <sebastiansca10@gmail.com>
2023-07-17 22:04:50 -0500

    DeleteUserCommandTests added

---
+++ RealEstate.Application.NUnitTests/Features/User/Commands/DeleteUserCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using RealEstate.Application.Contracts;
+using RealEstate.Application.Features.User.Commands;
+
+namespace RealEstate.Application.NUnitTests.Features.User.Commands
+{
+    [TestFixture]
+    public class DeleteUserCommandTests
+    {
+        private Mock<IIdentityService> _identityServiceMock;
+
+        [Test]
+        public async Task DeleteUserCommand_InputDeleteUserRequest_ReturnsTrue()
+        {
+            // Assert
+            _identityServiceMock = new Mock<IIdentityService>();
+            _identityServiceMock.Setup(x => x.DeleteUserAsync(It.IsAny<string>())).ReturnsAsync(true);
+            var request = new DeleteUserCommandRequest("user@test.com");
+            var handler = new DeleteUserCommandHanlder(_identityServiceMock.Object);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.TypeOf<bool>());
+                Assert.True(result);
+            });
+        }
+    }
+}
